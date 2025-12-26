578411a97a9ce445b0963450a81778db563a9c5d
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-12-26 13:48:37 +0600

    chore: add dummy post and category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,13 +17,36 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => 'password',
-                'email_verified_at' => now(),
-            ]
-        );
+        // User::firstOrCreate(
+        //     ['email' => 'test@example.com'],
+        //     [
+        //         'name' => 'Test User',
+        //         'password' => 'password',
+        //         'email_verified_at' => now(),
+        //     ]
+        // );
+
+        $categories = [
+            'Technology',
+            'Health',
+            'Science',
+            'Sports',
+            'Politics',
+            'Entertainment',
+        ];
+
+        foreach ($categories as $category) {
+            Category::firstOrCreate([
+                'name' => $category,
+            ]);
+        };
+
+        // actually seed by this line, that's why i don't need to make a seeder for post
+        // Post::factory(100)->create();
+
+        // this is how i can call other seeder from main seeder
+        // $this->call([
+        //     PostSeeder::class,
+        // ]);
     }
 }
